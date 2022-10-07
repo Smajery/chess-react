2e24edd13537378edbf8d5339fd9ac1c62a2cdfc
Smajery <vasiliew.sasha@gmail.com>
2022-10-07 11:48:58 +0200

    Board and Cells

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,36 @@
+import {Colors} from "../Colors";
+import logo from '../../assets/black-bishop.png'
+import {Cell} from "../Cell";
+
+export enum FigureName {
+    FIGURE = 'Фигура',
+    KING = 'Король',
+    KNIGHT = 'Конь',
+    PAW = 'Пешка',
+    QUEEN = 'Королева',
+    ROOK = 'Ладья',
+    BISHOPS = 'Слон',
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureName;
+    id: number;
+
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureName.FIGURE;
+        this.id = Math.random();
+    }
+
+    canMove(target: Cell) : boolean {
+        return true
+    }
+    moveFigure(target: Cell) {}
+}
